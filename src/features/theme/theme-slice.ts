3735d71c6f395f514c2ef6c7407e82fb80559645
Ivan Mikheev <avagardo@mail.ru>
2022-11-29 17:17:52 +0300

    Theme
    Another type

--- src/features/theme/theme-slice.ts
+++ src/features/theme/theme-slice.ts
@@ -1,14 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export type Theme = 'light' | 'dark';
 
 const themeSlice = createSlice({
-  name: '@@theme',
-  initialState: 'light' as Theme,
-  reducers: {
-    setTheme: (_, action) => action.payload,
-  }
+    name: '@@theme',
+    initialState: 'light' as Theme,
+    reducers: {
+        setTheme: (_, action: PayloadAction<Theme>) => action.payload,
+    }
 });
 
 export const {setTheme} = themeSlice.actions;
--- src/features/theme/use-theme.ts
+++ src/features/theme/use-theme.ts
@@ -1,17 +1,18 @@
-import {useSelector, useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react';
 
-import { setTheme } from './theme-slice';
+import {setTheme, Theme} from './theme-slice';
+import {selectTheme} from "./theme-selectors";
 
-export const useTheme = () => {
-  const dispatch = useDispatch();
-  const theme = useSelector(state => state.theme);
+export const useTheme = (): [Theme, () => void] => {
+    const dispatch = useDispatch();
+    const theme = useSelector(selectTheme);
 
-  const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
+    const toggleTheme = () =>  {dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))};
 
-  useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-  }, [theme]);
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
 
-  return [theme, toggleTheme]
+    return [theme, toggleTheme]
 }
